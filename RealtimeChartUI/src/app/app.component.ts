0400dbc46be5b551774fc6146cdb42b5b2d031e1
ersaumya <ersaumyarout@gmail.com>
2020-04-20 19:54:49 +0530

    chart component added

--- RealtimeChartUI/src/app/app.component.ts
+++ RealtimeChartUI/src/app/app.component.ts
@@ -8,6 +8,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  public chartOptions: any = {
+    scaleShowVerticalLines: true,
+    responsive: true,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+  public chartLabels: string[] = ['Real time data for the chart'];
+  public chartType: string = 'bar';
+  public chartLegend: boolean = true;
+  public colors: any[] = [
+    { backgroundColor: '#5491DA' },
+    { backgroundColor: '#E74C3C' },
+    { backgroundColor: '#82E0AA' },
+    { backgroundColor: '#E5E7E9' },
+  ];
+
   constructor(
     public signalrService: SignalrService,
     private http: HttpClient
